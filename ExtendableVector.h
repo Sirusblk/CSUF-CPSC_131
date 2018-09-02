ffb1ed227cc04009e8507a889225f8a5f8f859a8
David McLaren <sirusblk@gmail.com>
2018-09-02 14:56:22 -0700

    Changed ExtendableVector

--- ExtendableVector.h
+++ ExtendableVector.h
@@ -1,111 +1,156 @@
-//
-// ExtendableVector.h
-// ExtendableVector class using code from
-// Data Structures and Algorithms in C++, Goodrich, Tamassia, and Mount, 2nd Ed., 2011.
-//
-//
-
 #pragma once
 
 #include <stdexcept>
 #include <algorithm> // needed for std::max in ExtendableVector::insert()
-
 using namespace std;
 
-const int DEFAULT_VECTOR_CAPACITY = 100;
+const int DEFAULT_CAPACITY = 100;
 
-template <typename ELT>
+// Declaration
+template <typename T>
 class ExtendableVector {
-public:
-   // length copies of default_value
-   ExtendableVector(int length = DEFAULT_VECTOR_CAPACITY) {
-      if (length <= 0) throw invalid_argument("length <= 0");
-      capacity = length;
-      numberOfElements = 0;
-      elements = new ELT[length];
-   }
-   
-   // copy constructor
-   ExtendableVector(const ExtendableVector& a) {
-      capacity = a.capacity;
-      numberOfElements = a.numberOfElements;
-      elements = new ELT[numberOfElements];
-      for (int i = 0; i < numberOfElements; i++) {
-         elements[i] = a.elements[i]; }
-   }
-   
-   ~ExtendableVector() { delete[] elements; }
-   
-   // assignment operator from another extendable vector
-   ExtendableVector& operator=(const ExtendableVector& theVect) {
-      if (this != &theVect) {	// avoid self-assignment
-         delete[] elements;       // delete old array
-         capacity = theVect.capacity;  // set the capacity
-         elements = new ELT[capacity]; // allocate new array
-         numberOfElements = theVect.numberOfElements; // set the size
-         for (int i = 0; i < numberOfElements; i++) {
-            elements[i] = theVect.elements[i]; // copy the vector contents
-         }
-      }
-      return *this;
-   }
-   
-   int size() { // returns number of elements
-      return numberOfElements; }
-   
-   bool empty() { // is vector empty?
-      return (numberOfElements == 0); }
-   
-   int getCapacity() {
-      return capacity;
-   }
-   
-   ELT& operator[](int index) { // element at index i
-      return at(index);
-   }
-   
-   ELT& at(int index) { // element at index i
-      if ((index < 0) || (index >= numberOfElements)) {
-         throw range_error("index out of bounds"); }
-      return elements[index];
-   }
-   
-   void erase(int i) { // remove element at index i
-      for (int j = i+1; j < numberOfElements; j++) {
-         elements[j-1] = elements[j]; // shift elements down
-      }
-      numberOfElements--; // one fewer element
-   }
-   
-   void reserve(int N) { // reserve a least N slots
-      if (capacity >= N) { // already big enough
-         return;
-      }
-      ELT *newArray = new ELT[N]; // allocate bigger array
-      for (int j = 0; j < numberOfElements; j++) {
-         newArray[j] = elements[j]; // copy contents to new array
-      }
-      if (elements != nullptr) { // discard old array
-         delete [] elements;
-      }
-      elements = newArray; // have extendable array point to new array
-      capacity = N; // set new capacity
-   }
-   
-   void insert(int index, ELT value) {
-      if (index < 0) throw range_error("index out of bounds");
-      if (numberOfElements >= capacity) // overflow?
-         reserve(max(1, 2 * capacity)); // double current array size
-      for (int j = numberOfElements-1; j >= index; j--) {
-         elements[j+1] = elements[j]; // shift elements up
-      }
-      elements[index] = value;
-      numberOfElements++;
-   }
-   
 private:
-   int capacity;               // current array size
-   int numberOfElements;       // number of elements in vector
-   ELT *elements;              // array storing elements
+    int _capacity;                  // current max capacity
+    int _size;                      // number of elements in array
+    T* _array;                      // array storing elements
+
+public:
+    // Constructors
+    ExtendableVector();
+    ExtendableVector(int);
+    ExtendableVector(const ExtendableVector&);
+    ~ExtendableVector();
+
+    // Getters / Setters
+    T& get(int);
+    void insert(int, T);
+    void erase(int);
+    int getSize();
+    int getCapacity();
+    bool isEmpty();
+
+    // Helpers
+    void reserve(int);
+
+    // Overloaded Operators
+    ExtendableVector& operator=(const ExtendableVector&);
+    T& operator[](int);
 };
 
+
+// Implementation
+// Default Constructor
+template <typename T>
+ExtendableVector<T>::ExtendableVector() {
+    _capacity = DEFAULT_CAPACITY;
+    _size = 0;
+    _array = new T[_capacity];
+}
+
+// Constructor with initial capacity argument
+template <typename T>
+ExtendableVector<T>::ExtendableVector(int capacity) {
+    _capacity = capacity;
+    _size = 0;
+    _array = new T[_capacity];
+}
+
+// Copy Constructor
+template <typename T>
+ExtendableVector<T>::ExtendableVector(const ExtendableVector& input) {
+    _capacity = input._capacity;
+    _size = input._size;
+    _array = new T[_capacity];
+    for (int i = 0; i < _size; i++) {
+        _array[i] = input._array[i];
+    }
+}
+
+// Deconstructor
+template <typename T>
+ExtendableVector<T>::~ExtendableVector() {
+    delete[] _array;
+}
+
+// Getter
+template <typename T>
+T& ExtendableVector<T>::get(int index) {
+    if ((index < 0) || (index >= _size)) {
+        throw range_error("index out of bounds");
+    }
+    return _array[index];
+}
+
+template <typename T>
+void ExtendableVector<T>::insert(int index, T value) {
+    if ((index < 0)) {
+        throw range_error("index out of bounds");
+    }
+    // If at max capacity, double the capacity
+    if (_size >= _capacity) {
+        reserve(max(1, 2 * _capacity));
+    }
+    // Shift values up
+    for (int i = _size - 1; i >= index; i--) {
+        _array[i + 1] = _array[i];
+    }
+    _array[index] = value;
+    _size++;
+}
+
+template <typename T>
+void ExtendableVector<T>::erase(int index) {
+    // Shift values down
+    for (int i = index + 1; i < _size; i++) {
+        _array[i - 1] = _array[i];
+    }
+    _size--;
+}
+
+template <typename T>
+int ExtendableVector<T>::getSize() {
+    return _size;
+}
+
+template <typename T>
+int ExtendableVector<T>::getCapacity() {
+    return _capacity;
+}
+
+template <typename T>
+bool ExtendableVector<T>::isEmpty() {
+    return (_size == 0);
+}
+
+template <typename T>
+void ExtendableVector<T>::reserve(int capacity) {
+    if (capacity > _capacity) {
+        T* new_array = new T[capacity];
+        // Copy values to new array
+        for (int i = 0; i < _size; i++) {
+            new_array[i] = _array[i];
+        }
+        delete[] _array;
+        _array = new_array;
+        _capacity = capacity;
+    }
+}
+
+template <typename T>
+ExtendableVector<T>& ExtendableVector<T>::operator=(const ExtendableVector& rhs) {
+    if (this != &rhs) {
+        delete[] _array;
+        _capacity = rhs._capacity;
+        _size = rhs._size;
+        _array = new T[_capacity];
+        for (int i = 0; i < _size; i++) {
+            _array[i] = rhs._array[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+T& ExtendableVector<T>::operator[](int index) {
+    return get(index);
+}
