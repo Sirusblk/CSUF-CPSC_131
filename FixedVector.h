0b5a5cf14b4ba90c7ea2da2f017f43ee7fb4ca0d
David McLaren <sirusblk@gmail.com>
2018-09-02 14:45:44 -0700

    Changed FixedVector

--- FixedVector.h
+++ FixedVector.h
@@ -1,57 +1,116 @@
 #pragma once
 
 #include <stdexcept>
-
 using namespace std;
 
-template <typename ELT>
+
+// Declaration
+template <typename T>
 class FixedVector {
 private:
-	int _length;
-	ELT *_array;
+    int _size;
+    T* _array;
 
 public:
-	// length copies of default_value
-	FixedVector(int length) {
-		if (length < 0) throw invalid_argument("length < 0");
-		_length = length;
-		_array = new ELT[_length];
-	}
+    // Constructors
+    FixedVector();
+    FixedVector(int);
+    FixedVector(const FixedVector&);
+    ~FixedVector();
 
-	// default constructor: empty vector
-	FixedVector() {
-		_length = 0;
-		_array = new ELT[0];
-	}
+    // Getters / Setters
+    T& get(int);
+    void set(int, T);
+    int getSize();
+    bool isEmpty();
 
-	// copy constructor
-	FixedVector(const FixedVector& a) {
-		_length = a._length;
-		_array = new ELT[_length];
-		for (int i = 0; i < _length; i++)
-			_array[i] = a._array[i];
-	}
-
-	~FixedVector() { delete[] _array; }
-
-	int length() { return _length; }
-
-	bool is_empty() { return (_length == 0); }
-
-	ELT& get(int index) {
-		if ((index < 0) || (index >= _length)) throw range_error("index out of bounds");
-		return _array[index];
-	}
-
-	void set(int index, ELT value) {
-		if (is_empty()) throw invalid_argument("array is empty");
-		if ((index < 0) || (index >= _length)) throw range_error("index out of bounds");
-		_array[index] = value;
-	}
-
-	ELT& operator[](int index) {
-		if ((index < 0) || (index >= _length)) throw range_error("index out of bounds");
-		return get(index);
-	}
+    // Overloaded Operators
+    FixedVector& operator=(const FixedVector&);
+    T& operator[](int);
 };
 
+
+// Implementation
+// Default Constructor: empty vector
+template <typename T>
+FixedVector<T>::FixedVector() {
+    _size = 0;
+    _array = new T[_size];
+}
+
+// Constructor with initial size argument
+template <typename T>
+FixedVector<T>::FixedVector(int size) {
+    if (size < 0) {
+        throw invalid_argument("size is less than 0");
+    }
+    _size = size;
+    _array = new T[_size];
+}
+
+// Copy Constructor
+template <typename T>
+FixedVector<T>::FixedVector(const FixedVector<T>& input) {
+    _size = input._size;
+    _array = new T[_size];
+    for (int i = 0; i < _size; i++) {
+        _array[i] = input._array[i];
+    }
+}
+
+// Deconstructor
+template <typename T>
+FixedVector<T>::~FixedVector() {
+    delete[] _array;
+}
+
+// Getter
+template <typename T>
+T& FixedVector<T>::get(int index) {
+    if ((index < 0) || (index >= _size)) {
+        throw range_error("index out of bounds");
+    }
+    return _array[index];
+}
+
+// Setter
+template <typename T>
+void FixedVector<T>::set(int index, T value) {
+    if (isEmpty()) {
+        throw invalid_argument("array is empty");
+    }
+    if ((index < 0) || (index >= _size)) {
+        throw range_error("index out of bounds");
+    }
+    _array[index] = value;
+}
+
+template <typename T>
+int FixedVector<T>::getSize() {
+    return _size;
+}
+
+template <typename T>
+bool FixedVector<T>::isEmpty() {
+    return (_size == 0);
+}
+
+// Overloaded Assignment Operator
+template <typename T>
+FixedVector<T>& FixedVector<T>::operator=(const FixedVector<T>& rhs) {
+    if (this != &rhs) {
+        delete[] _array;
+        _size = rhs._size;
+        _array = new T[_size];
+        for (int i = 0; i < _size; i++) {
+            _array[i] = rhs._array[i];
+        }
+    }
+    return *this;
+}
+
+// Overloaded Array-Access Operator
+template <typename T>
+T& FixedVector<T>::operator[](int index) {
+    return get(index);
+}
--- FixedVector_main.cpp
+++ FixedVector_main.cpp
@@ -3,42 +3,39 @@
 #include <string>
 
 #include "FixedVector.h"
-
 using namespace std;
 
 class Student {
 private:
-	string _name;
-	int _nsemesters;
+    string _name;
+    int _num_of_semesters;
 
 public:
-	void print() {
-		cout << "Name:" << _name << "; No. of semesters=" << _nsemesters << endl;
-	}
-	void setName(string name) {
-		_name = name;
-	}
-	void setNSemesters(int n) {
-		_nsemesters = n;
-	}
-	void updateNSemesters() {
-		_nsemesters++;
-	}
+    void print() {
+        cout << "Name:" << _name << "; No. of semesters=" << _num_of_semesters << endl;
+    }
+    void setName(string name) {
+        _name = name;
+    }
+    void setNSemesters(int n) {
+        _num_of_semesters = n;
+    }
+    void updateNSemesters() {
+        _num_of_semesters++;
+    }
 };
 
 int main() {
+    FixedVector<Student> students(10);
 
-	FixedVector<Student> students(10);
+    Student s;
+    for (int i = 0; i < 5; i++) {
+        s.setName("Student_" + to_string(i));
+        s.setNSemesters(2);
+        students.set(i, s);
+    }
 
-	Student s;
-	for (int i = 0; i < 5; i++) {
-		s.setName("Student_" + to_string(i));
-		s.setNSemesters(2);
-		students.set(i, s);
-	}
-
-	for (int i = 0; i < students.length(); ++i) {
-		students.get(i).print();
-	}
-	// system("pause");
+    for (int i = 0; i < students.getSize(); ++i) {
+        students.get(i).print();
+    }
 }
